d42f6177fd7eaca4177ec78045951c2cb1176dec
Veress Máté <43212975+theefues@users.noreply.github.com>
2019-02-26 09:51:38 +0100

    Update l33t.c

--- turing/l33t.c
+++ turing/l33t.c
@@ -1,42 +1,47 @@
 #include <stdio.h>
 
-int main() {
-    int i = 0;
-    char string[50];
-
-    printf("Enter your string: ");
-
-    fgets(string, 50, stdin);
-
-    while (string[i] != '\0') {
-
-        switch (string[i]) {
-
-        case 'A':
-        case 'a':
-                string[i] = '4';
-                break;
-        case 'I':
-        case 'i':
-                string[i] = '1';
-                break;
-        case 'E':
-        case 'e':
-                string[i] = '3';
-                break;
-        case 'T':
-        case 't':
-                string[i] = '7';
-                break;
-        case 'O':
-        case 'o':
-                string[i] = '0';
-                break;
-        default:
-                break;
-        }
-        i++;
-    }
-    printf("Your string in 1337: %s", string);
-    return 0;
+int main(int argc, char **argv) {
+    if(argc < 2) {
+            printf("error: no input file\n");
+    } else {
+            int i = 0;
+            int k = 14400;
+            FILE *stri = fopen(argv[1], "r");
+            char string[k];
+            fgets(string, k, stri);
+            while (string[i] != '\0') {
+                switch (string[i]) {
+                    case 'A':
+                    case 'a':
+                            string[i] = '4';
+                            break;
+                    case 'I':
+                    case 'i':
+                            string[i] = '1';
+                            break;
+                    case 'E':
+                    case 'e':
+                            string[i] = '3';
+                            break;
+                    case 'T':
+                    case 't':
+                            string[i] = '7';
+                            break;
+                    case 'O':
+                    case 'o':
+                            string[i] = '0';
+                            break;
+                    case 'S':
+                    case 's':
+                            string[i] = '5';
+                            break;
+                    default:
+                            break;
+                }
+                i++;
+            }
+            printf("Your string in 1337: %s", string);
+            fclose(stri);
+            return 0;
+            }
 }
