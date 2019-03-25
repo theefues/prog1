b8efd714732eb3ddc3dc4dff7e91cc9f799a38ef
Veress Máté <43212975+theefues@users.noreply.github.com>
2019-03-25 19:16:41 +0100

    Update decun.c

--- chomsky/decun.c
+++ chomsky/decun.c
@@ -4,9 +4,12 @@ int main() {
         int szam;
         printf("Adj meg egy számot: ");
         if (scanf("%d", &szam)) {
-                for(int i = 0; i < szam; i++)
-                        printf("|");
+                for(int i = 0; i < szam; i++){
+                        if(i % 5 == 0 && i != 0)
+                        printf(" ");
 
+                        printf("|");
+                }
                 printf("\n");
         } else {
                 printf("Kérlek, számot adj meg.\n");
