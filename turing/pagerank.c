7c42ab78245cb324cbc8c72276cecfd77722e66d
Veress Máté <43212975+theefues@users.noreply.github.com>
2019-03-26 11:09:29 +0100

    Update pagerank.c

--- turing/pagerank.c
+++ turing/pagerank.c
@@ -1,56 +1,69 @@
 #include <stdio.h>
 #include <math.h>
-#include <stdlib.h>
 
 void
-kiir (double tomb[], int db)
-{
-int i;
-for (i=0; i<db; i++)
-printf("PageRank [%d]: %lf\n", i, tomb[i]);
+kiir (double tomb[], int db){
+	
+	int i;
+	
+	for (i=0; i<db; ++i){
+		printf("%f\n",tomb[i]);
+	}
 }
 
-double tavolsag(double pagerank[],double pagerank_temp[],int db)
-{
-double tav = 0.0;
-int i;
-for(i=0;i<db;i++)
-tav +=abs(pagerank[i] - pagerank_temp[i]);
-return tav;
+double
+tavolsag (double PR[], double PRv[], int n){
+	
+	int i;
+	double osszeg=0;
+	
+	for (i = 0; i < n; ++i)
+		osszeg += (PRv[i] - PR[i]) * (PRv[i] - PR[i]);
+	
+	return sqrt(osszeg);
 }
 
-int main(void)
-{
-double L[4][4] = {
-{0.0, 0.0, 1.0 / 3.0, 0.0},
-{1.0 / 2.0, 0.0, 1.0 / 3.0, 0.0},
-{1.0 / 2.0, 0.0, 0.0, 1.0},
-{0.0, 1.0, 1.0 / 3.0, 0.0}
-};
-
-
-double PR[4] = {0.0, 0.0, 0.0, 0.0};
-double PRv[4] = {1.0 / 4.0, 1.0 / 4.0, 1.0 / 4.0, 1.0 / 4.0};
-
-long int i,j,h;
-i=0; j=0; h=5;
-
-for (;;)
-{
-for(i=0;i<4;i++)
-PR[i] = PRv[i];
-for (i=0;i<4;i++)
-{
-double temp=0;
-for (j=0;j<4;j++)
-temp+=L[i][j]*PR[j];
-PRv[i]=temp;
+void
+pagerank(double T[4][4]){
+	double PR[4] = { 0.0, 0.0, 0.0, 0.0 }; //ebbe megy az eredmény
+	double PRv[4] = { 1.0/4.0, 1.0/4.0, 1.0/4.0, 1.0/4.0}; //ezzel szorzok
+	
+	int i, j;
+	
+	for(;;){
+		
+		// ide jön a mátrix művelet		
+		
+		for (i=0; i<4; i++){
+			PR[i]=0.0;
+			for (j=0; j<4; j++){
+				PR[i] = PR[i] + T[i][j]*PRv[j];
+			}
+		}
+	
+			if (tavolsag(PR,PRv,4) < 0.0000000001) 
+				break;
+		
+		// ide meg az átpakolás PR-ből PRv-be
+			
+			for (i=0;i<4; i++){
+				PRv[i]=PR[i];
+			}	
+	}
+	
+	kiir (PR, 4);
 }
 
-if ( tavolsag(PR,PRv, 4) < 0.00001)
-break;
-}
-kiir (PR,4);
-return 0;
+int main (void){
+	double L[4][4] = {
+		{0.0,  0.0,      1.0/3.0,  0.0},
+		{1.0,  1.0/2.0,  1.0/3.0,  1.0},
+		{0.0,  1.0/2.0,  0.0,      0.0},
+		{0.0,  0.0, 	 1.0/3.0,  0.0}
+	};	
+	
+	printf("PageRank\n");
+	pagerank(L);
 
+	return 0;
 }
