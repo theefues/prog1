fcb2a8b0ecaaca88ef31d4750b814390faa96f01
Veress Máté <43212975+theefues@users.noreply.github.com>
2019-03-29 21:01:42 +0100

    Update complex.cpp

--- mandelbrot/complex.cpp
+++ mandelbrot/complex.cpp
@@ -1,58 +1,78 @@
+#include <iostream>
 #include "png++/png.hpp"
 #include <complex>
 
-const int N = 500;
-const int M = 500;
-const double MAXX = 0.7;
-const double MINX = -2.0;
-const double MAXY = 1.35;
-const double MINY = -1.35;
-
-void GeneratePNG(const int tomb[N][M])
+int
+main ( int argc, char *argv[] )
 {
-    png::image< png::rgb_pixel > image(N, M);
-    for (int x = 0; x < N; x++)
+
+  int szelesseg = 1920;
+  int magassag = 1080;
+  int iteraciosHatar = 255;
+  double a = -1.9;
+  double b = 0.7;
+  double c = -1.3;
+  double d = 1.3;
+
+  if ( argc == 9 )
     {
-        for (int y = 0; y < M; y++)
-        {
-            image[x][y] = png::rgb_pixel(tomb[x][y], tomb[x][y], tomb[x][y]);
-        }
+      szelesseg = atoi ( argv[2] );
+      magassag =  atoi ( argv[3] );
+      iteraciosHatar =  atoi ( argv[4] );
+      a = atof ( argv[5] );
+      b = atof ( argv[6] );
+      c = atof ( argv[7] );
+      d = atof ( argv[8] );
     }
-    image.write("kimenet.png");
-}
-
-int main()
-{
-    int tomb[N][M];
-
-    double dx = (MAXX - MINX) / N;
-    double dy = (MAXY - MINY) / M;
-
-    std::complex<double> C, Z, Zuj;
-
-    int iteracio;
-
-    for (int i = 0; i < M; i++)
+  else
     {
-        for (int j = 0; j < N; j++)
+      std::cout << "Hasznalat: ./3.1.2 fajlnev szelesseg magassag n a b c d" << std::endl;
+      return -1;
+    }
+
+  png::image < png::rgb_pixel > kep ( szelesseg, magassag );
+
+  double dx = ( b - a ) / szelesseg;
+  double dy = ( d - c ) / magassag;
+  double reC, imC, reZ, imZ;
+  int iteracio = 0;
+
+  std::cout << "Szamitas\n";
+
+  // j megy a sorokon
+  for ( int j = 0; j < magassag; ++j )
+    {
+      // k megy az oszlopokon
+
+      for ( int k = 0; k < szelesseg; ++k )
         {
-            real(C) = MINX + j * dx;
-            imag(C) = MAXY - i * dy;
 
-            Z = 0;
-            iteracio = 0;
+          // c = (reC, imC) a halo racspontjainak
+          // megfelelo komplex szam
 
-            while(abs(Z) < 2 && iteracio++ < 255)
+          reC = a + k * dx;
+          imC = d - j * dy;
+          std::complex<double> c ( reC, imC );
+
+          std::complex<double> z_n ( 0, 0 );
+          iteracio = 0;
+
+          while ( std::abs ( z_n ) < 4 && iteracio < iteraciosHatar )
             {
-                Zuj = Z*Z+C;
-                Z = Zuj;
+              z_n = z_n * z_n + c;
+
+              ++iteracio;
             }
 
-            tomb[i][j] = 256 - iteracio;
+          kep.set_pixel ( k, j,
+                          png::rgb_pixel ( iteracio%255, (iteracio*iteracio)%255, 0 ) );
         }
+
+      int szazalek = ( double ) j / ( double ) magassag * 100.0;
+      std::cout << "\r" << szazalek << "%" << std::flush;
     }
 
-    GeneratePNG(tomb);
+  kep.write ( argv[1] );
+  std::cout << "\r" << argv[1] << " mentve." << std::endl;
 
-    return 0;
 }
